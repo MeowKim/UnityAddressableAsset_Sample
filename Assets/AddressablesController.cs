6e971d872acb4df7f0df761dda833ade61e0d303
MeowKim <ariesmilk51@gmail.com>
2020-07-03 11:40:41 +0900

    Add AddressablesController

---
+++ Assets/AddressablesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AddressablesController : MonoBehaviour
+{
+	public string _label;
+	private Transform _parent;
+	private List<GameObject> _createdObjs { get; } = new List<GameObject>();
+
+	private void Start()
+	{
+		_parent = GameObject.Find("Example Assets").transform;
+		Instantiate();
+	}
+
+	private async void Instantiate()
+	{
+		await AddressablesLoader.InitAssets(_label, _createdObjs, _parent);
+	}
+}
