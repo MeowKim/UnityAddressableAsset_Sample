e38ae6dbb1ce69a84fd7544ce6f870b0b8b2dadb
MeowKim <ariesmilk51@gmail.com>
2020-07-03 11:37:40 +0900

    Add AddressablesLoader

---
+++ Assets/Scripts/AddressablesLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class AddressablesLoader
+{
+	public static async Task InitAssets<T>(string label, List<T> createdObjs, Transform parent)
+		where T : Object
+	{
+		var locations = await Addressables.LoadResourceLocationsAsync(label).Task;
+
+		foreach (var location in locations)
+		{
+			createdObjs.Add(await Addressables.InstantiateAsync(location, parent).Task as T);
+		}
+	}
+}
